03ab9fe9b437c1ed3f3ed9bfd1ab45c5b58ee8ad
Francisco <franciscogthiesen@gmail.com>
2017-01-22 00:22:27 -0200

    Improving Notebook

---
+++ rmqDP.cpp
@@ -0,0 +1,10 @@
+#define better(a,b) A[a]<A[b]?(a):(b)
+int make_dp(int n) { // N log N
+    for(int i = 0; i < n; ++i) H[i][0]=i;
+    for(int l=0,k; (k=1<<l) < n; l++) for(int i=0;i+k<n;i++)
+        H[i][l+1] = better(H[i][l], H[i+k][l]);
+}
+int query_dp(int a, int b) {
+	int l = __lg(b-a);
+	return better(H[a][l], H[b-(1<<l)+1][l]);
+}
